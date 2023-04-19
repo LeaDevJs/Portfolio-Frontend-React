c25021fb23b712e621b09bcc34be98b6af7092a6
leansoda99 <86275758+leansoda99@users.noreply.github.com>
2023-04-19 18:13:46 -0300

    responsive a las imagenes del portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -19,43 +19,50 @@ function Portfolio(){
         <div className="contenido-seccion">
             <h2>PORTFOLIO</h2>
             <div className="galeria">
-                <a href="https://github.com/leansoda99/Attack-on-Titan">
+                <div>
                     <div className="proyecto">
-                        <img src={Img1} alt=""/>
+                        <a href="https://github.com/leansoda99/Attack-on-Titan">
+                            <img src={Img1} alt=""/>
+                        </a>
                         <div className="overlay">
                             <h3>Attack on Titan</h3>
                             <p>Videojuego desarrollado con Java</p>
                         </div>
                     </div>
-                </a>
-                <a href="https://github.com/leansoda99/JuegoBarbarianna">
+                    <button onClick={handleClickImg1}>Vista previa
+                        <span className="overlay"></span>
+                    </button>
+                </div>
+                <div>
                     <div className="proyecto">
-                        <img src={Img2} alt=""/>
+                        <a href="https://github.com/leansoda99/JuegoBarbarianna">   
+                            <img src={Img2} alt=""/>
+                        </a>
                         <div className="overlay">
                             <h3>Barbarianna</h3>
                             <p>Videojuego desarrollado con Java</p>
                         </div>
                     </div>
-                </a>
-                <a href="https://github.com/leansoda99/PortfolioFrontendAngular">
+                    <button onClick={handleClickImg2}>Vista previa
+                        <span className="overlay"></span>
+                    </button>
+                </div>
+                
+                <div>
                     <div className="proyecto">
-                        <img src={Img3} alt=""/>
+                        <a href="https://github.com/leansoda99/PortfolioFrontendAngular">
+                            <img src={Img3} alt=""/>
+                        </a>
                         <div className="overlay">
                             <h3>Portfolio Frontend</h3>
                             <p>Portfolio frontend desarrollado con Angular</p>
                         </div>
                     </div>
-                </a>
-                <button onClick={handleClickImg1}>Vista previa
-                    <span className="overlay"></span>
-                </button>
-
-                <button onClick={handleClickImg2}>Vista previa
-                    <span className="overlay"></span>
-                </button>
-                <button onClick={handleClickImg3}>Vista previa
-                    <span className="overlay"></span>
-                </button>
+                    <button onClick={handleClickImg3}>Vista previa
+                        <span className="overlay"></span>
+                    </button>
+                </div>
+                  
             </div>
         </div>
     </section>
