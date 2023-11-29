45908d2cdcda0b6ab61392ce1ddd03dd95d98125
Leandro Zapata <lean.carp1435@gmail.com>
2023-11-29 20:53:40 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,50 +1,33 @@
 import "./App.css"
 import { Route, Routes } from 'react-router-dom';
-import Header from "./components/Header";
-import Principal from "./components/Principal";
-import SobreMi from "./components/SobreMi";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import Portfolio from "./components/Portfolio";
-import Footer from "./components/Footer";
-import HeaderEn from "./components/HeaderEn";
-import PrincipalEn from "./components/PrincipalEn";
-import SobreMiEn from "./components/SobreMiEn";
-import SkillsEn from "./components/SkillsEn";
-import ExperienceEn from "./components/ExperienceEn";
-import PortfolioEn from "./components/PortfolioEn";
-import FooterEn from "./components/FooterEn";
+
+import HomeEs from "./components/HomeEs";
+
+
+import HomeEn from "./components/HomeEn";
 function App() {
+  
+  
   return (
     <>
     
      <Routes>
       
+     
         
         <Route path="/es" element={
-          <>
-            <Header />
-            <Principal />
-            <SobreMi />
-            <Skills />
-            <Experience />
-            <Portfolio />
-            <Footer />
-          </>
+          
+         
+          <HomeEs />
           }>
         </Route>
-
+          
        
         <Route path="/en" element={
           <>
-            
-            <HeaderEn />
-                 <PrincipalEn />
-                 <SobreMiEn />
-                 <SkillsEn />
-                 <ExperienceEn />
-                 <PortfolioEn />
-                 <FooterEn /> 
+          
+          <HomeEn />
+                 
           </>
           }>
         </Route>
@@ -52,17 +35,14 @@ function App() {
         
         <Route path="/" element={
         <>
-          <Header />
-          <Principal />
-          <SobreMi />
-          <Skills />
-          <Experience/>
-          <Portfolio />
-          <Footer />
+          
+          <HomeEs />
           </>
           }>
         </Route>
-      
+        
+        
+        
         </Routes>
         
     </>
